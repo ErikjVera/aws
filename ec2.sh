a6088b08585c5199a7600a59dc5e084685d77dae
ErikjVera <133960465+ErikjVera@users.noreply.github.com>
2023-10-24 23:18:04 +0200

    ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,7 +1,9 @@
 aws ec2 run-instances \
 --image-id ami-050406429a71aaa64 \
---count 1  \
+--count 1 \
 --instance-type t2.micro \
 --key-name vockey \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2ERIK}]' \
+--subnet-id subnet-0123456789abcdef0 \  # Asegúrate de que sea una subnet pública
+--associate-public-ip-address \  # Esto asigna una IP pública a la instancia
 --region us-east-1
