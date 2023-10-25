d00014c6da57af819b46fd880606d282ed87dc4a
ErikjVera <133960465+ErikjVera@users.noreply.github.com>
2023-10-25 11:51:26 +0200

    ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,19 +1,19 @@
-AWS_ID_GrupoSeguridad_EC2ERIK=$(aws ec2 create-security-group \
---group-name 'grupoEC2ERIK' \
---description 'grupo para habilitar ssh' \
---output text)
+AWS_ID_GrupoSeguridad_EC2ERIK2=$(
+  aws ec2 create-security-group \
+  --group-name 'SecGroupEC2ERIK2' \
+  --description 'Permitir conexiones SSH' \
+  --output text
+)
 
-# Corrección: añadir la regla de ingreso al grupo de seguridad
 aws ec2 authorize-security-group-ingress \
---group-id $AWS_ID_GrupoSeguridad_EC2ERIK \
+--group-id $AWS_ID_GrupoSeguridad_EC2ERIK2 \
 --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "0.0.0.0/0", "Description": "Allow SSH"}]}]'
 
-# Corrección: usar la variable AWS_ID_GrupoSeguridad_EC2ERIK en el comando run-instances
 aws ec2 run-instances \
---image-id ami-0dbc3d7bc646e8516 \
---count 1 \
---instance-type t2.micro \
---region us-east-1 \
+--image-id ami-050406429a71aaa64 \
+--count 1  \
+--instance-type m1.small \
 --key-name vockey \
---tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2ERIK}]' \
---security-group-ids $AWS_ID_GrupoSeguridad_EC2ERIK
+--region us-east-1 \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2ERIK2}]' \
+--security-group-ids $AWS_ID_GrupoSeguridad_ERIK2
