2f6b299968f6095e7976f9e49da66610bc8533ef
ErikjVera <133960465+ErikjVera@users.noreply.github.com>
2023-10-25 11:30:22 +0200

    ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,3 +1,12 @@
+AWS_ID_GgrupoSeguridad_EC2ERIK=$(aws ec2 create-security-group \
+--group-name 'grupoEC2ERIK' \
+--description 'grupo para habilitar ssh' \
+--output text)
+
+aws ec2 authorize-security-group-ingress \
+--group-id $AWS_ID_GrupoSeguridad_EC2ERIK 
+--ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{CdrIp": "0.0.0.0/0", "Description": "Allow SSH" }]}]'
+
 aws ec2 run-instances \
 --image-id ami-0dbc3d7bc646e8516 \
 --count 1 \
@@ -5,5 +14,4 @@ aws ec2 run-instances \
 --region us-east-1 \
 --key-name vockey \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2ERIK}]' \
---associate-public-ip-address \
---security-group-ids sg-05be8783dceb1eec8 \
+--security-group-ids AWS_ID_GrupoSeguridad_EC2ERIK  \
