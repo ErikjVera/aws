1c6289ae69f1d135eda129d33bb8ac2ba417c460
ErikjVera <133960465+ErikjVera@users.noreply.github.com>
2023-10-25 10:38:38 +0200

    ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -2,9 +2,10 @@ aws ec2 run-instances \
 --image-id ami-050406429a71aaa64 \
 --count 1 \
 --instance-type t2.micro \
+--region us-east-1 \
 --key-name vockey \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2ERIK}]' \
 --subnet-id subnet-053c6279f007c462d \  # Reemplaza con la ID de tu subnet pública
 --associate-public-ip-address \
 --security-group-ids sg-05be8783dceb1eec8 \  # Reemplaza con el ID de tu grupo de seguridad vockey
---region us-east-1
+
